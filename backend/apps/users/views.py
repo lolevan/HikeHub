4d5e5f33e864f60f21502d1e5919811c996522d4
Ivan <vanechka-nikitin-2004@mail.ru>
2025-03-26 14:40:03 +0800

    Add logic for user and path for auth

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, serializers
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    """Регистрация нового пользователя"""
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
