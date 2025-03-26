b6bd9cdb2c45f15316d9ab78dfb84b0a52936eb4
Ivan <vanechka-nikitin-2004@mail.ru>
2025-03-26 14:02:49 +0800

    Create template frontend/public files

---
+++ frontend/public/pages/hike/_id.vue
@@ -0,0 +1,6 @@
+<template>
+  <section class="section">
+    <h1 class="title">Поход #{{ $route.params.id }}</h1>
+    <!-- здесь будет описание и форма заявки -->
+  </section>
+</template>
---
+++ frontend/public/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <section class="section">
+    <h1 class="title">Походы</h1>
+    <!-- здесь будет поиск и список -->
+  </section>
+</template>
